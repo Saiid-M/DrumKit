d877e4c997dbcb9e90a2044c31d09da8fdb2f676
saidmaaliki <109993366+saidmaaliki@users.noreply.github.com>
2024-07-19 22:39:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
   makeSound(buttonInnerHTML);
+  buttonAnimation(buttonInnerHTML);
   });
 }
-document.addEventListener("keydown",function(event){makeSound(event.key);})
+document.addEventListener("keydown",function(event){makeSound(event.key);
+  buttonAnimation(event.key);
+})
 
 function makeSound(key){
   switch (key) {
@@ -41,3 +44,8 @@ function makeSound(key){
   }
 }
 
+function buttonAnimation(currentKey){
+  var activeButton=document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){activeButton.classList.remove("pressed");},100)
+}
