5f729f6c40ad4360837b9eb54a03a8944cc1fbb3
saidmaaliki <109993366+saidmaaliki@users.noreply.github.com>
2024-07-19 20:06:54 +0300

    yes

--- index.js
+++ index.js
@@ -1,44 +1,13 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
-    switch (buttonInnerHTML) {
-      case "w":
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-        break;
-      case "a":
-        var tom1 = new Audio("sounds/tom-2.mp3");
-        tom1.play();
-        break;
-      case "s":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-      case "d":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-      case "j":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-      case "k":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-      case "l":
-        var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-        default:
-        break;
-    }
+  makeSound(buttonInnerHTML);
   });
 }
-document.addEventListener("keydown", function (event) {makeSound(event.key);});
+document.addEventListener("keydown",function(event){makeSound(event.key);})
 
-function makeSound(key) {
-  switch (buttonInnerHTML) {
+function makeSound(key){
+  switch (key) {
     case "w":
       var audio = new Audio("sounds/tom-1.mp3");
       audio.play();
@@ -67,7 +36,7 @@ function makeSound(key) {
       var kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
-    default:
+      default:
       break;
   }
 }
